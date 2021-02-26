4766ce5494f2dbb8b726a54ecf31054e8b4173be
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-26 12:23:21 +0000

    added starting code use etc

--- src/NamelessLeaf/ReplyWith/Main.php
+++ src/NamelessLeaf/ReplyWith/Main.php
@@ -2,3 +2,14 @@
 namespace NamelessLeaf\Main;
 
 use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\Command;
+use pocketmie\command\CommandSender;
+
+use pocketmine\eventListener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+  
+}
